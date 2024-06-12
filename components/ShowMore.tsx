d5e5bcce50dd197d48b15e0f3632eb362d4a9387
MD2311 <pmduc2311@gmail.com>
2024-06-12 10:14:03 +0700

    fixing page changing position on url params changes

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -9,7 +9,7 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const handleNavigation = () => {
     const newLimit = (pageNumber + 1) * 10;
     const newPathName = updateSearchParams("limit", `${newLimit}`);
-    router.push(newPathName);
+    router.push(newPathName, { scroll: false });
   };
 
   return (
